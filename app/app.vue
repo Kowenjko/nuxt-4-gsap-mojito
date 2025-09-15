14e689a4d8b43e3cfd902005e46487fc6c0208c2
Kowenjko <ortoswt@gmail.com>
2025-09-15 13:29:54 +0300

    add public

--- app/app.vue
+++ app/app.vue
@@ -1,17 +1,13 @@
+<script lang="ts" setup>
+useHead({
+	titleTemplate: (titleChunk) => {
+		return titleChunk ? `${titleChunk} - Gsap Mojito` : 'Gsap Mojito'
+	},
+})
+</script>
+
 <template>
-	<div class="container mx-auto">
-		<article>
-			<h1>Garlic bread with cheese: What the science tells us</h1>
-			<p>
-				For years parents have espoused the health benefits of eating garlic bread with cheese to their children, with
-				the food earning such an iconic status in our culture that kids will often dress up as warm, cheesy loaf for
-				Halloween.
-			</p>
-			<p>
-				But a recent study shows that the celebrated appetizer may be linked to a series of rabies cases springing up
-				around the country.
-			</p>
-			<!-- ... -->
-		</article>
-	</div>
+	<nuxt-layout>
+		<nuxt-page />
+	</nuxt-layout>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,13 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	css: ['~/assets/css/main.css'],
-	modules: ['v-gsap-nuxt'],
+	modules: ['v-gsap-nuxt', '@nuxt/image', '@nuxt/fonts', '@formkit/auto-animate/nuxt'],
+	fonts: {
+		families: [
+			{ name: 'Mona+Sans', provider: 'google' },
+			{ name: 'DM Serif Text', provider: 'google' },
+		],
+	},
 	vite: {
 		plugins: [tailwindcss()],
 	},
